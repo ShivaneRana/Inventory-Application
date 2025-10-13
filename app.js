d3cfe8403953a66c833908374acb0d6adc265f3e
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-13 14:32:15 +0530

    chore: added basic comments

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("node:path");
 
-dotenv.config({quiet:false,debug:true});
+dotenv.config({quiet:true,debug:false});
+
+//create and epress instance
 const app = express();
 
+//set ejs as view engine template 
 app.set("view engine","ejs");
+//set views as default path for views template tho its views by default
 app.set("views",path.join(__dirname,"views"));
 
+// for dealing with form
 app.set(express.urlencoded({extended:true}));
+// set publice for static files
 app.set(express.static(path.join(__dirname,"public")));
 
-
-
 app.get("/",(req,res) => {
-    console.log("root route");
     res.status(200).send("root route has been reached");
 });
 
