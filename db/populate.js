4f8cc633cd6301b12372b0434bfb51c3d29daade
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-14 15:57:49 +0530

    chore: added a way to automate populating local/online db

--- db/populate.js
+++ db/populate.js
@@ -1,16 +1,31 @@
 #! /usr/bin/env node
 const { Client } = require("pg");
 
-const SQL = ``
+const dotenv = require("dotenv");
+const {argv} = require("node:process");
+const path = require("node:path")
 
+dotenv.config({path:path.resolve(__dirname,"../.env"),quiet:true});
+
+if(argv[2] === 'dev'){
+    console.log("populate developer database");
+
+}else if(argv[2] === "prod"){
+    console.log("populate production database");
+
+}
+
+let SQL = ``;
 
 async function main(){
 
-    const client = new Client({connectionString: ""});
+    const client = new Client({connectionString: "postgresql://localhost:5432/manga_db"});
 
     try{
         console.log("starting script........");
-
+        await client.connect();
+        await client.query(SQL);
+        await client.end();
         console.log("done...........");
 
     }catch(err){
@@ -19,6 +34,7 @@ async function main(){
         client.end();
     }
 }
+
 
 module.exports = main;
 
