1cadad2d9306c271ee21759e057d369eadc42171
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-18 15:35:29 +0530

    chore: added boilerplate code for handling http request to index router

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,3 +3,27 @@ const {query,body, validationResult} = require("express-validator");
 exports.indexGetHomePage = (req,res) => {
     res.status(200).send("This will be the homepage");
 };
+
+exports.indexGetCategoriesList = (req,res) => {
+    res.status(200).send("List of all the categories");
+}
+
+exports.indexGetLanguagesList = (req,res) => {
+    res.status(200).send("List of all the languages");
+}
+
+exports.indexGetAuthorsList = (req,res) => {
+    res.status(200).send("List of all the authors");
+}
+
+exports.indexGetMangasList = (req,res) => {
+    res.status(200).send("List of all the mangas");
+}
+
+exports.indexGetPublishersList = (req,res) => {
+    res.status(200).send("List of all the publishers");
+}
+
+exports.indexGetGenresList = (req,res) => {
+    res.status(200).send("List of all the genres");
+}
