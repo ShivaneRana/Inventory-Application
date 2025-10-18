0db71a721bae40892b4caf29acfb6663a114b0bc
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-18 15:36:31 +0530

    chore: added controllers for get request to specific path

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,5 +3,11 @@ const indexController = require("../controllers/indexController.js")
 const indexRouter = Router();
 
 indexRouter.get("/",indexController.indexGetHomePage);
+indexRouter.get("/all",indexController.indexGetCategoriesList);
+indexRouter.get("/languages",indexController.indexGetLanguagesList);
+indexRouter.get("/genres",indexController.indexGetGenresList);
+indexRouter.get("/authors",indexController.indexGetAuthorsList);
+indexRouter.get("/mangas",indexController.indexGetMangasList);
+indexRouter.get("/publishers",indexController.indexGetPublishersList);
 
 module.exports = indexRouter;
