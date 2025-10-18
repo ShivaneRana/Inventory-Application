068343e7da15a0b314c485c804f2fc6ae85c74b8
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-18 15:27:11 +0530

    chore: created a pool for executing queries on the database

---
+++ db/pool.js
@@ -0,0 +1,8 @@
+const {Pool} = require("pg");
+const dotenv = require("dotenv");
+const path = require("node:path");
+dotenv.config({ path: path.resolve(__dirname, "../.env"), quiet: true });
+
+const DATABASE_URL = process.env.DATABASE_DEV;
+
+module.exports = new Pool({connectionString: DATABASE_URL});
