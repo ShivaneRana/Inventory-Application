1282cd1f25b9806c010b141fb01e42e43ac862fd
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-18 20:39:53 +0530

    fix: change set to use for urlencoded and static middleware function

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.urlencoded({ extended: true }));
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
 app.use((err,req,res,next) => {
   console.error(err);
-  res.send(err.statusCode || 500).send(err.message);
+  res.status(err.statusCode || 500).send(err.message);
 })
 
 const PORT = process.env.PORT;
