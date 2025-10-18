363cbb74a3320eac5b545d17f7158994fe4951e9
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-18 14:23:44 +0530

    chore: a new router added to handle request in root. error handling middleware function also added

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("node:path");
+const indexRouter = require("./routers/indexRouter.js");
 
 dotenv.config({ quiet: true, debug: false });
-
-//create and epress instance
 const app = express();
 
-//set ejs as view engine template
 app.set("view engine", "ejs");
-//set views as default path for views template tho its views by default
 app.set("views", path.join(__dirname, "views"));
-
-// for dealing with form
 app.set(express.urlencoded({ extended: true }));
-// set publice for static files
 app.set(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.status(200).send("root route has been reached");
-});
+app.get("/", indexRouter);
 
 const PORT = process.env.PORT;
+
+app.use((err,req,res,next) => {
+  console.error(err);
+  console.log(err.message);
+})
+
 app.listen(PORT, (err) => {
   if (err) {
     console.error(err);
