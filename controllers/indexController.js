4e109e630b0fc8d8cfe3ff1f90f1a8fba9bc22ee
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-18 18:46:04 +0530

    feat: sending get request to paticular url return data about corresponding table

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,5 @@
 const {query,body, validationResult} = require("express-validator");
+const db = require("../db/queries.js");
 
 exports.indexGetHomePage = (req,res) => {
     res.status(200).render("index");
@@ -8,22 +9,32 @@ exports.indexGetCategoriesList = (req,res) => {
     res.status(200).render("categories");
 }
 
-exports.indexGetLanguagesList = (req,res) => {
+exports.indexGetLanguagesList = async (req,res) => {
+    const rows = await db.getAllLanguages();
+    console.log(rows);
     res.status(200).render("languages");
 }
 
-exports.indexGetAuthorsList = (req,res) => {
+exports.indexGetAuthorsList = async (req,res) => {
+    const rows = await db.getAllAuthors();
+    console.log(rows);
     res.status(200).send("authors");
 }
 
-exports.indexGetMangasList = (req,res) => {
+exports.indexGetMangasList = async (req,res) => {
+    const rows = await db.getAllMangas();
+    console.log(rows);
     res.status(200).send("mangas");
 }
 
-exports.indexGetPublishersList = (req,res) => {
+exports.indexGetPublishersList = async (req,res) => {
+    const rows = await db.getAllPublishers();
+    console.log(rows);
     res.status(200).send("publishers");
 }
 
-exports.indexGetGenresList = (req,res) => {
+exports.indexGetGenresList = async (req,res) => {
+    const rows = await db.getAllGenres();
+    console.log(rows);
     res.status(200).send("genres");
 }
