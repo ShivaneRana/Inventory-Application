3c6d63b88e44e18c470dd70ceeff09aafadbd379
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-24 18:03:06 +0530

    style: prettier used

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,39 +1,39 @@
-const {query,body, validationResult} = require("express-validator");
-const db = require("../db/queries.js");
+const { query, body, validationResult } = require("express-validator")
+const db = require("../db/queries.js")
 
-exports.indexGetHomePage = (req,res) => {
-    res.status(200).render("index");
-};
-
-exports.indexGetCategoriesList = (req,res) => {
-    res.status(200).render("categories");
+exports.indexGetHomePage = (req, res) => {
+    res.status(200).render("index")
 }
 
-exports.indexGetLanguagesList = async (req,res) => {
-    const rows = await db.getAllLanguages();
-    console.log(rows);
-    res.status(200).render("languages");
+exports.indexGetCategoriesList = (req, res) => {
+    res.status(200).render("categories")
 }
 
-exports.indexGetAuthorsList = async (req,res) => {
-    const rows = await db.getAllAuthors();
-    console.log(rows);
-    res.status(200).render("authors",{rows:rows});
+exports.indexGetLanguagesList = async (req, res) => {
+    const rows = await db.getAllLanguages()
+    console.log(rows)
+    res.status(200).render("languages")
 }
 
-exports.indexGetMangasList = async (req,res) => {
-    const rows = await db.getAllMangas();
-    console.log(rows);
-    res.status(200).render("mangas");
+exports.indexGetAuthorsList = async (req, res) => {
+    const rows = await db.getAllAuthors()
+    console.log(rows)
+    res.status(200).render("authors", { rows: rows })
 }
 
-exports.indexGetPublishersList = async (req,res) => {
-    const rows = await db.getAllPublishers();
-    console.log(rows);
-    res.status(200).render("publishers");
+exports.indexGetMangasList = async (req, res) => {
+    const rows = await db.getAllMangas()
+    console.log(rows)
+    res.status(200).render("mangas")
 }
 
-exports.indexGetGenresList = async (req,res) => {
-    const rows = await db.getAllGenres();
-    res.status(200).render("genres",{rows: rows});
+exports.indexGetPublishersList = async (req, res) => {
+    const rows = await db.getAllPublishers()
+    console.log(rows)
+    res.status(200).render("publishers")
+}
+
+exports.indexGetGenresList = async (req, res) => {
+    const rows = await db.getAllGenres()
+    res.status(200).render("genres")
 }
