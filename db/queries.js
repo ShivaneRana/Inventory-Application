841e30bf4dd9fb65330b1ca235460558c95107b5
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-10-25 17:24:44 +0530

    fix: all queries are now ordered

--- db/queries.js
+++ db/queries.js
@@ -2,26 +2,26 @@ const pool = require("./pool.js");
 
 // return all mangas and also inventories
 exports.getAllMangas = async () => {
-    const { rows } = await pool.query("SELECT * FROM mangas as m JOIN inventories as i ON m.id = i.id");
+    const { rows } = await pool.query("SELECT * FROM mangas as m JOIN inventories as i ON m.id = i.id ORDER BY m.name");
     return rows;
 };
 
 exports.getAllLanguages = async () => {
-    const { rows } = await pool.query("SELECT * FROM languages");
+    const { rows } = await pool.query("SELECT * FROM languages ORDER BY name");
     return rows;
 };
 
 exports.getAllPublishers = async () => {
-    const { rows } = await pool.query("SELECT * FROM publishers");
+    const { rows } = await pool.query("SELECT * FROM publishers ORDER BY name");
     return rows;
 };
 
 exports.getAllAuthors = async () => {
-    const { rows } = await pool.query("SELECT * FROM authors");
+    const { rows } = await pool.query("SELECT * FROM authors ORDER BY first_name");
     return rows;
 };
 
 exports.getAllGenres = async () => {
-    const { rows } = await pool.query("SELECT * FROM genres");
+    const { rows } = await pool.query("SELECT * FROM genres ORDER BY name");
     return rows;
 };
