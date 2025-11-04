8ad9de3933a0a8e141625a02105a41a304ad3e8e
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-11-04 22:30:40 +0530

    chore: prettier used

--- controllers/publisherController.js
+++ controllers/publisherController.js
@@ -1,38 +1,50 @@
-const { body, param, validationResult, matchedData } = require("express-validator");
+const {
+    body,
+    param,
+    validationResult,
+    matchedData,
+} = require("express-validator");
 const db = require("../db/queries.js");
 
 validationObject = [
-    body("publisher_name").trim()
-    .matches(/^[a-zA-Z0-9\s]+$/).withMessage("Publisher name only contain alphabet letters or numbers."),
-    body("publisher_country").trim()
-    .matches(/^[a-zA-Z\s]+$/).withMessage("Publisher country can only contain alphabet letters.")
-]
+    body("publisher_name")
+        .trim()
+        .matches(/^[a-zA-Z0-9\s]+$/)
+        .withMessage(
+            "Publisher name only contain alphabet letters or numbers."
+        ),
+    body("publisher_country")
+        .trim()
+        .matches(/^[a-zA-Z\s]+$/)
+        .withMessage("Publisher country can only contain alphabet letters."),
+];
 
 exports.getPublishersList = async (req, res) => {
     const rows = await db.getAllPublishers();
-    res.status(200).render("publishers", { rows: rows ,flag:false});
+    res.status(200).render("publishers", { rows: rows, flag: false });
 };
 
 exports.getAddPublisher = async (req, res) => {
     const rows = await db.getAllPublishers();
-    res.status(200).render("publishers", { rows: rows, flag: true});
+    res.status(200).render("publishers", { rows: rows, flag: true });
 };
 
 exports.postAddPublisher = [
     validationObject,
-    async (req,res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        const rows = await db.getAllPublishers();
-        return res.status(404).render("publishers",{
-            rows:rows,
-            flag:true,
-            errors:errors.array()
-        });
-    }
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const rows = await db.getAllPublishers();
+            return res.status(404).render("publishers", {
+                rows: rows,
+                flag: true,
+                errors: errors.array(),
+            });
+        }
 
-    const {publisher_name, publisher_country} = matchedData(req);
-    console.log(publisher_name);
-    console.log(publisher_country);
-    return res.redirect("/publishers");
-}];
+        const { publisher_name, publisher_country } = matchedData(req);
+        console.log(publisher_name);
+        console.log(publisher_country);
+        return res.redirect("/publishers");
+    },
+];
