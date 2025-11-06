78db633a1c3fbf6c006ea2b97619d1e28784da32
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-11-06 13:53:44 +0530

    feat: added form and form validation

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,7 +1,37 @@
 const { body, param, validationResult } = require("express-validator");
 const db = require("../db/queries.js");
 
+const validationObject = [
+    body("author_name").trim().matches(/^[a-zA-Z0-9\s]+$/)
+    .withMessage(
+        "Author name can only contain alphabets and numeric value"
+    ),
+    body("author_age").trim().isNumeric()
+    .withMessage("Author age can only contain numeric value"),
+    body("author_country").trim().matches(/^[a-zA-Z\s]+$/)
+    .withMessage("Author country can only contain alphabet letters."),
+]
+
 exports.getAuthorsList = async (req, res) => {
     const rows = await db.getAllAuthors();
-    res.status(200).render("authors", { rows: rows });
+    res.status(200).render("authors", { rows: rows ,flag:false});
 };
+
+exports.getAddAuthor = async (req, res) => {
+    const rows = await db.getAllAuthors();
+    res.status(200).render("authors", { rows: rows ,flag:true});
+};
+
+exports.postAddAuthor = [
+    validationObject,
+    async (req,res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            const rows = await db.getAllAuthors();
+            return res.status(404).render("authors",{rows:rows, flag: true, errors: errors.array()})
+        }
+        
+        const {author_name,author_country,author_gender,author_age} = req.body;
+        return res.status(200).redirect("/authors");
+    }
+]
--- routers/authorRouter.js
+++ routers/authorRouter.js
@@ -4,5 +4,7 @@ const authorController = require("../controllers/authorController.js");
 const authorRouter = Router();
 
 authorRouter.get("/", authorController.getAuthorsList);
+authorRouter.get("/add", authorController.getAddAuthor);
+authorRouter.post("/add", authorController.postAddAuthor);
 
 module.exports = authorRouter;
