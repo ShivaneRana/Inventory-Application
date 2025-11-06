aa3f72738dd12b02c5a23d46840c4f49a9a6ba65
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-11-06 13:03:17 +0530

    feat: form and form validation added

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,7 +1,35 @@
-const { body, param, validationResult } = require("express-validator");
+const { body, param, validationResult, matchedData } = require("express-validator");
 const db = require("../db/queries.js");
+
+const validationObject = [
+    body("genre_name").trim()
+    .matches(/^[a-zA-Z\s]+$/).withMessage("Genre name should be in alphabetic letters.")
+]
 
 exports.getGenresList = async (req, res) => {
     const rows = await db.getAllGenres();
-    res.status(200).render("genres", { rows: rows });
+    res.status(200).render("genres", { rows: rows ,flag:false});
 };
+
+exports.getAddGenre = async (req, res) => {
+    const rows = await db.getAllGenres();
+    res.status(200).render("genres", { rows: rows ,flag:true});
+};
+
+exports.postAddGenre = [
+    validationObject,
+    async (req,res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            const rows = await db.getAllGenres();
+            return res.status(400).render("genres",{
+                rows:rows,
+                flag:true,
+                errors: errors.array()
+            })
+        }
+        const {genre_name} = matchedData(req);
+        console.log(genre_name);
+        return res.status(200).redirect("/genres");
+    }
+]
