9bd2050142d8bc3a168594feadc68b164a73adff
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-11-12 19:28:27 +0530

    feat: added backend form-validation check

--- controllers/mangaController.js
+++ controllers/mangaController.js
@@ -1,5 +1,25 @@
-const { body, param, validationResult } = require("express-validator");
+const { body, param, validationResult ,matchedData} = require("express-validator");
 const db = require("../db/queries.js");
+
+const validationObject = [
+    body("manga_name").trim().notEmpty()
+    .withMessage("Name cannot be empty"),
+    body("manga_rating").trim()
+    .isFloat({min:0,max:10})
+    .withMessage("Rating can only contain numerical or floating value between 0.1 or 10"),
+    body("manga_price").trim()
+    .isFloat({min:0.1})
+    .withMessage("Price can only contain numerical or floating value."),
+    body("manga_chapter_number").trim()
+    .isInt({min:1})
+    .withMessage("Chapter number can only contain numerical value"),
+    body("manga_volume_number").trim()
+    .isInt({min:1})
+    .withMessage("Volume number can only contain numerical value"),
+    body("manga_quantity").trim()
+    .isInt({min:1})
+    .withMessage("Quantity can only contain numerical value"),
+]
 
 exports.getMangasList = async (req, res) => {
     const rows = await db.getAllMangas();
@@ -11,7 +31,15 @@ exports.getAddManga = async (req, res) => {
     return res.status(200).render("mangas", { rows: rows, flag: true });
 };
 
-exports.postAddManga = async (req, res) => {
+exports.postAddManga = [
+    validationObject,
+    async(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const rows = await db.getAllMangas();
+        return res.status(400).render("mangas",{rows: rows,flag:true,errors:errors.array()});
+    }
+
     const {
         manga_name,
         manga_image_url,
@@ -37,7 +65,7 @@ exports.postAddManga = async (req, res) => {
     );
 
     return res.status(200).redirect("/mangas");
-};
+}];
 
 exports.postDeleteManga = async (req, res) => {
     const { id } = req.params;
