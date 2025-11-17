ad3987b9a0261d2952a463525bc8a1441bf5414f
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-11-17 16:39:26 +0530

    feat: functionality to update publishres added

--- controllers/publisherController.js
+++ controllers/publisherController.js
@@ -53,3 +53,34 @@ exports.postDeletePublisher = async (req, res) => {
     await db.deletePublisher(id);
     return res.status(200).redirect("/publishers");
 };
+
+exports.getUpdatePublisher = async(req,res) => {
+    const {id} = req.params;
+    const rows = await db.getAllPublishers();
+    const value = rows.find(item => item.publisher_id === Number(id));
+    return res.status(200).render("publishers", { rows: rows, flag: true ,update:true,value:value});
+}
+
+exports.postUpdatePublisher = [
+    validationObject,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const rows = await db.getAllPublishers();
+            const { id } = req.params;
+            const value = rows.find(item => item.publisher_id === Number(id));
+            return res.status(404).render("publishers", {
+                rows: rows,
+                flag: true,
+                update:true,
+                value:value,
+                errors: errors.array(),
+            });
+        }
+
+        const { id } = req.params;
+        const { publisher_name, publisher_country } = matchedData(req);
+        await db.updatePublisher(id,publisher_name,publisher_country);
+        return res.status(200).redirect("/publishers");
+    },
+];
--- routers/publisherRouter.js
+++ routers/publisherRouter.js
@@ -6,5 +6,7 @@ publisherRouter.get("/", publisherController.getPublishersList);
 publisherRouter.get("/add", publisherController.getAddPublisher);
 publisherRouter.post("/add", publisherController.postAddPublisher);
 publisherRouter.post("/delete/:id", publisherController.postDeletePublisher);
+publisherRouter.get("/update/:id", publisherController.getUpdatePublisher);
+publisherRouter.post("/update/:id", publisherController.postUpdatePublisher);
 
 module.exports = publisherRouter;
