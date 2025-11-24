17f3b9bfd6e7ed594cb7ca0e81856f4d2693891b
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-11-24 09:29:36 +0530

    chore: switched to production database

--- db/pool.js
+++ db/pool.js
@@ -3,6 +3,6 @@ const dotenv = require("dotenv");
 const path = require("node:path");
 dotenv.config({ path: path.resolve(__dirname, "../.env"), quiet: true });
 
-const DATABASE_URL = process.env.DATABASE_DEV;
+const DATABASE_URL = process.env.DATABASE_PROD;
 
 module.exports = new Pool({ connectionString: DATABASE_URL });
