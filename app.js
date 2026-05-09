cbeb69c0f857c14b709100a18baf956fe3fe7ee6
Shivane <72935198+ShivaneRana@users.noreply.github.com>
2026-05-09 16:48:06 +0530

    refactor: added a fallback port

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
     res.status(err.statusCode || 500).send(err.message || 'Internal server error');
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, (err) => {
     if (err) {
         console.error(err);
